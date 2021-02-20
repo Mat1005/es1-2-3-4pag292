31ceff9e38360a230f6cc0e8fcdf5f76f3d860ef
Matteo-Zaccarelli <63998743+Mat1005@users.noreply.github.com>
2021-02-20 07:41:41 +0100

    Update es2.py

--- es2.py
+++ es2.py
@@ -8,7 +8,7 @@ class Atleta:
         self.visitaMedica = visitaMedica
         self.squadra = squadra
     def info(self):
-        print("Nome:",self.name,"Età:",self.age,"Altezza:",self.height,"Peso:",self.weight,"Squadra:",self.squadra)
+        print("Nome:",self.name,"Età:",self.age,"Altezza:",self.height,"Peso:",self.weight)
         if self.visitaMedica == True:
             print("L'atleta",self.name,"ha ricevuto la visita medica.")
         if self.visitaMedica == False:
